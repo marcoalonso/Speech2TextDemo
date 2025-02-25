639dd8cf60e9fede6f90f48bb25211defb29934b
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2025-02-25 10:06:40 -0600

    Initial Commit

---
+++ SpeechToTextDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeechToTextDemo
+//
+//  Created by Marco Alonso Rodriguez on 25/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeechToTextDemo/SpeechToTextDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextDemoApp.swift
+//  SpeechToTextDemo
+//
+//  Created by Marco Alonso Rodriguez on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechToTextDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
